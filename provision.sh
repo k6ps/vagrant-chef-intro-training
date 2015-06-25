cd76f30c5db0d2275157b4772752b1a509d08992
k6ps <taavi.tanavsuu@mail.ee>
2015-06-25 16:24:29 +0300

    Added tomcat admin user configuration

--- provision.sh
+++ provision.sh
@@ -7,3 +7,11 @@ else
    echo === !! Ei õnnestunud Tomcati installida !! ===
    exit 1
 fi
+
+echo === konfime Tomcati admin kasutaja ===
+if sudo cp /vagrant/tomcat-users.xml.template /etc/tomcat7/tomcat-users.xml && sudo service tomcat7 restart; then
+   echo === Tomcati admin kasutaja edukalt konfitud ===
+else
+   echo === !! Ei õnnestunud Tomcati admin kasutajat konfida !! ===
+   exit 1
+fi
