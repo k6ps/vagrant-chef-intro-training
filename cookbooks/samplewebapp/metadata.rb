bc6581020a8446c68dd7711c199dc2d266f66fe1
k6ps <taavi.tanavsuu@mail.ee>
2015-06-26 11:03:43 +0300

    Corrected metadata

--- cookbooks/samplewebapp/metadata.rb
+++ cookbooks/samplewebapp/metadata.rb
@@ -1,7 +1,7 @@
 name             'samplewebapp'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Nortal'
+maintainer_email 'sampler@somecompany.com'
 license          'All rights reserved'
-description      'Installs/Configures samplewebapp'
+description      'Installs platform for SampleWebApp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
